d0e7a90db35edac0b57b2c1fff37ad0903cbf5a6
Oleg Stepovoy <s.olegys0801@gmail.com>
2023-11-29 16:29:16 +0300

    update input char limit

--- app/genShortText.py
+++ app/genShortText.py
@@ -4,7 +4,7 @@ from gptModel import MyModel
 def short_text(model: MyModel):
     st.title("Генерация короткого текста")
     st.write("Введите начало текста, а модель вам поможет продолжить")
-    input_text = st.text_area("Введите текст", height=100, key='1', max_chars=200)
+    input_text = st.text_area("Введите текст", height=100, key='1', max_chars=1000)
     if st.button("Сгенерировать!"):
         with st.spinner("Генерация..."):
             result_text = model.gen_short_text(input_text)
